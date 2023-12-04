a9bf3d96219d881d9a710ff63796a014429fdd00
dannieldev <danielcrespin.dev.com>
2023-12-04 17:58:09 -0600

    Cambios en opacidad de boton

--- book.js
+++ book.js
@@ -7,25 +7,29 @@ $('.chapterSpines__images').hover(function () {
     $(this).siblings().css('flex', '1');
 });
 
+console.log("Hello world!");
+
+
 // Utilizando Vanilla JS
 document.querySelectorAll('.chapterSpines__images').forEach(function (element) {
-    
-    var boton = document.querySelectorAll("chapterSpines__button");
+
     element.addEventListener('mouseover', function () {
         this.parentNode.childNodes.forEach(function (sibling) {
             if (sibling.nodeType === 1 && sibling !== element) {
                 sibling.style.flex = '1';
                 
+                
             }
         });
         element.style.flex = '4';
-
+        var boton = document.querySelectorAll("chapterSpines__button");
 
         if (boton.style.opacity === "0" || boton.style.opacity === "") {
             // Si el elemento está oculto, mostrarlo
             boton.style.filter = "blur(0)"; // Eliminar el desenfoque
             boton.style.opacity = "1"; // Mostrar el elemento
-          } else {
+            console.alert("Help")
+        } else {
             // Si el elemento está visible, ocultarlo
             boton.style.filter = "blur(10px)"; // Aplicar desenfoque
             boton.style.opacity = "0"; // Ocultar el elemento
